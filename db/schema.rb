b2b8bfc842710414e574aaeacb63d82bd1fc7d6e
Gourav <gauravdec25@gmail.com>
2020-01-20 19:21:43 +0530

    Heroku config

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_01_20_064238) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "todos", force: :cascade do |t|
     t.string "title"
     t.text "notes"
